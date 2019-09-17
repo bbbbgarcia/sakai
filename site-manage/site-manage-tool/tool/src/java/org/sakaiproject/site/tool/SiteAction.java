27f3db8a4ce16514039d962c3b9ff282572eea4e
Jose Cebellán Sánchez <josecebe@gmail.com>
2019-09-17 14:00:28 +0200

    FARM5: coursedates2

--- site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage/site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -4414,7 +4414,7 @@ public class SiteAction extends PagedResourceActionII {
 				HELPER_ID + ".siteId", ((Site) getStateSite(state)).getId());*/
 
 		// launch the helper
-		startHelper(data.getRequest(), "sakai.coursedates.helper");
+		startHelper(data.getRequest(), "sakai.coursedates");
 
 	} // doCourseDatesHelper
 
